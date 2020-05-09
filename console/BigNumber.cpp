59fa9846756856c508cc23a769d9c1e1006f922a
khaphan132 <64716505+khaphan132@users.noreply.github.com>
2020-05-09 10:59:18 +0700

    Update BigNumber.cpp

--- console/BigNumber.cpp
+++ console/BigNumber.cpp
@@ -1,104 +1,28 @@
-#include "BigNumber.h"
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
 
-BigNumber::BigNumber()
+class BigNumber
 {
-	m_size = 50;
-	m_num.resize(m_size, '0');
-}
+private:
+	string m_num;
+public:
+	BigNumber();
+	BigNumber(string);
+	BigNumber(const BigNumber& other);
+	~BigNumber();
 
-BigNumber::BigNumber(string str)
-{
-	m_size = 50;
-	m_num.resize(m_size);
+	void fixNum();
+	void print();
+	string getNum();
+	BigNumber operator + (const BigNumber& other);
+	void multi2();//nhan voi 2
+	void divide2();
+	void multiPow2(int m);//nhan voi 2 mu m
 
-	for (int i = 0; i <= str.length(); i++) {
-		m_num[m_size + i - str.length() - 1] = str[i];
-	}
-}
+	BigNumber& operator = (const BigNumber& other);
+	BigNumber& operator = (const int& x);//gan bang 1 so nguyen
 
-BigNumber::BigNumber(const BigNumber& other)
-{
-	m_size = 50;
-	m_num.resize(m_size, '0');
+};
 
-	for (int i = 0; i <= m_size; i++) {
-		m_num[i] = other.m_num[i];
-	}
-}
-
-BigNumber::~BigNumber()
-{
-
-}
-
-void BigNumber::print()
-{
-	cout << m_num << endl;
-}
-
-string BigNumber::getNum()
-{
-	return m_num;
-}
-
-BigNumber BigNumber::operator + (const BigNumber& other) {
-	bool carry = false;
-	BigNumber rs;
-	for (int i = m_size - 1; i >= 0; i--) {
-		char temp = this->m_num[i] + other.m_num[i] - '0';
-		if (carry) {
-			temp++;
-			carry = false;
-		}
-
-		if (temp > '9') {
-			temp -= 10;
-			carry = true;
-		}
-
-		rs.m_num[i] = temp;
-	}
-
-	return rs;
-}
-
-void BigNumber::multi2()
-{
-	*this = *this + *this;
-}
-
-void BigNumber::multiPow2(int m)
-{
-	for (int i = 1; i <= m; i++) {
-		this->multi2();
-	}
-}
-
-bool BigNumber::operator < (const BigNumber& other)
-{
-	for (int i = 0; i < m_size; i++) {
-		if (this->m_num[i] < other.m_num[i])
-			return true;
-	}
-	return false;
-}
-
-BigNumber& BigNumber::operator=(const BigNumber& other)
-{
-	if (this != &other) {
-		for (int i = 1; i < m_size; i++) {
-			this->m_num[i] = other.m_num[i];
-		}
-	}
-	return *this;
-}
-
-BigNumber& BigNumber::operator=(const int& x)
-{
-	unsigned int temp = x;
-	for (int i = m_size - 1; i >= 0; i--) {
-		m_num[i] = (temp % 10) + '0';
-		temp /= 10;
-	}
-	return *this;
-}
